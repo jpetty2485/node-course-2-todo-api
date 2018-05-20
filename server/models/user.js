21bad48e2dadaf1c963585f2f9dd9956d169fe98
Josh <j.petty2485@gmail.com>
2018-05-20 19:06:00 -0400

    Added DELETE /users/me/token route

--- server/models/user.js
+++ server/models/user.js
@@ -52,7 +52,17 @@ UserSchema.methods.generateAuthToken = function() {
   });
 };
 
-UserSchema.statics.findByToken = function(token) {
+UserSchema.methods.removeToken = function (token) {
+  var user = this;
+
+  return user.update({
+    $pull: {
+      tokens: {token}
+    }
+  });
+};
+
+UserSchema.statics.findByToken = function (token) {
   var User = this;
   var decoded;
 
